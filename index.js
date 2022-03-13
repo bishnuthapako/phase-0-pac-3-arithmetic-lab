ce10730e825b3419cf8af4c64593b39f1b0f1ed3
Bishnu <bishnuthapako@gmail.com>
2022-03-12 22:03:24 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,104 @@
+function add(){
+    
+}
+add();
+
+function subtract(){
+
+}
+subtract()
+
+function multiply(){
+
+}
+multiply()
+
+function multiply(){
+
+}
+multiply()
+
+function divide(){
+
+}
+divide()
+
+function add(a,b){
+    return (a+b);
+    
+}
+add();
+
+function subtract(a,b){
+    return (a-b)
+
+}
+subtract()
+
+function multiply(a,b){
+    return (a*b)
+
+}
+multiply()
+
+function divide(a,b){
+    return (a/b)
+
+}
+divide()
+
+
+function increment(n){
+    return (n+=1)
+}
+let count = 0;
+function decrement(count){
+    return (count-=1)
+}
+
+
+function makeInt(n){
+    return parseInt(n,10)
+}
+
+function preserveDecimal(n){
+    console.log(parseFloat("2.222"));
+    console.log(parseFloat("hfafhaa"));
+    return parseFloat(n)
+}
+
+
+// makeInt("2", 10)
+
+// function preserveDecimal(n){
+//     return parseFloat(n);
+// }
+// preserveDecimal("80.123999");
+
+
+// function makeInt(n, base){
+//     const parsed = parseInt(n,base)
+//     return parsed;
+//  }
+
+
+/*
+
+function roughScale(x, base) {
+    const parsed = parseInt(x, base);
+    if (isNaN(parsed)) { return 0; }
+    return parsed * 100;
+  }
+  
+  console.log(roughScale(' 0xF', 16));
+  // expected output: 1500
+  
+  console.log(roughScale('321', 2));
+  // expected output: 0
+
+  */
+  
+
+
+
+
